94e41ab8cd6cf7c30c566338b684f7511c3d863c
Lou8 <lou8@mail.com>
2025-07-08 21:07:07 +0300

    Установка пакетов Vue 3, TypeScript, Pinia, Quasar UI

---
+++ main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+})
+
+app.mount('#app')
